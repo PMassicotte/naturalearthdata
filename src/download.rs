7285ba73786792838a2f9bd3cdc6134333911281
Philippe Massicotte <pmassicotte@hotmail.com>
2024-09-16 06:54:18 -0400

    feat(Cargo.toml): add arrow dependency with ffi feature for enhanced functionality chore(Cargo.toml): update geoarrow dependency to use latest git version for latest features and fixes refactor(download.rs): replace RecordBatchReader with Table for improved data handling and conversion

--- src/download.rs
+++ src/download.rs
@@ -1,11 +1,10 @@
 use crate::types::scales::Scale;
 use crate::types::traits::Downloadable;
-
 use gdal::Dataset;
 use geoarrow::error::GeoArrowError;
 use geoarrow::io::gdal::read_gdal;
 use geoarrow::io::RecordBatchReader;
-
+use geoarrow::table::Table;
 /// Downloads and reads a dataset from a given downloadable type and scale.
 ///
 /// # Arguments
@@ -26,14 +25,14 @@ pub fn ne_download<T: Downloadable>(
 ) -> Result<RecordBatchReader, GeoArrowError> {
     let url = downloadable_type.get_url(&scale);
 
-    // Should poke to see if the url exists?
-
     let dataset = Dataset::open(url)?;
 
     // Get the layer (assuming there is only one layer)
     let mut layer = dataset.layer(0)?;
 
+    // Using None will get all rows, specifying a number will create many batch wwith of that size
     let reader = read_gdal(&mut layer, None)?;
+    let table: Table = reader.try_into()?;
 
-    Ok(reader.into())
+    Ok(table.into())
 }
